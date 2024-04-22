c9a182e38e4d86dd8bb5a25a7691ef2e391b95df
B-AnhTu <tubuianh2017@gmail.com>
2024-04-22 16:34:49 +0700

    Create Model

---
+++ app/Http/Controllers/CrudPhoneController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Hash;
+use Session;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CrudPhoneController extends Controller
+{
+    //Add Product
+    public function addProduct($item) {
+        
+    }
+}
